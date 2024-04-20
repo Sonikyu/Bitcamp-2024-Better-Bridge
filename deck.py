fcb48a1c77b4939edaa7b352a6abb7b3bce63359
Max Park <parkr.m465@gmail.com>
2024-04-19 23:10:39 -0400

    Creation of base classes
    Added Card, Deck, Rank, and Suit classes

---
+++ deck.py
@@ -0,0 +1,8 @@
+from enum import Enum
+class Deck:
+    cards[54]
+    def __init__ ():
+        for rank in Rank():
+            for suit in Suit():
+                cards.append(Card(rank, suit))
+        
