5abf8eb5dbe9e208c2e73e96393f48c11498960d
JPR3 <jpr3@riganati.com>
2024-04-20 15:03:34 -0400

    Updated Card to store position and image

--- Render.py
+++ Render.py
@@ -3,7 +3,6 @@ from Rank import Rank
 from Suit import Suit
 import os
 import pygame
-# pygame.init()
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 750
@@ -19,8 +18,8 @@ def draw(hand, pile):
     #Draw the player hand
     hand_offset = (SCREEN_WIDTH - ((len(hand) + 1) * (CARD_WIDTH * 2 / 3))) / 2
     loc_offset = 0
-    for card in hand:
-        draw_card(screen, card, hand_offset + loc_offset*CARD_WIDTH * 2/3, HAND_Y)
+    for card in hand:       
+        screen.blit(card.image, (hand_offset + loc_offset*CARD_WIDTH * 2/3, HAND_Y))
         loc_offset += 1
     #Draw the pile
     draw_pile(screen, pile)
@@ -42,23 +41,17 @@ def get_png_from_card(card):
     output += ".png"
     return os.path.join("Cards", output)
 
-#Draws an individual card at x,y
-def draw_card(screen, card, x, y):
-    img_path = get_png_from_card(card)
-    img = pygame.image.load(img_path)
-    screen.blit(img, (x,y))
-
 #Draws the pile in the center of the screen, pile MUST be length 4 with None for any position not yet filled
 def draw_pile(screen, pile):
     #SOUTH
     if pile[0] != None:
-        draw_card(screen, pile[0], SCREEN_WIDTH / 2 - CARD_WIDTH / 2, SCREEN_HEIGHT /2)
+        screen.blit(pile[0].image, (SCREEN_WIDTH / 2 - CARD_WIDTH / 2, SCREEN_HEIGHT /2))
     #WEST
     if pile[1] != None:
-        draw_card(screen, pile[1], SCREEN_WIDTH / 2 - CARD_WIDTH * 1.5, SCREEN_HEIGHT /2 - CARD_HEIGHT * .75)
+        screen.blit(pile[1].image, (SCREEN_WIDTH / 2 - CARD_WIDTH * 1.5, SCREEN_HEIGHT /2 - CARD_HEIGHT * .75))
     #NORTH
     if pile[2] != None:
-        draw_card(screen, pile[2], SCREEN_WIDTH / 2 - CARD_WIDTH / 2, SCREEN_HEIGHT /2 - CARD_HEIGHT * 1.5) 
+        screen.blit(pile[2].image, (SCREEN_WIDTH / 2 - CARD_WIDTH / 2, SCREEN_HEIGHT /2 - CARD_HEIGHT * 1.5))
     #EAST
     if pile[3] != None:
-        draw_card(screen, pile[3], SCREEN_WIDTH / 2 + CARD_WIDTH * .5, SCREEN_HEIGHT /2 - CARD_HEIGHT * .75)
+        screen.blit(pile[3].image, (SCREEN_WIDTH / 2 + CARD_WIDTH * .5, SCREEN_HEIGHT /2 - CARD_HEIGHT * .75))
