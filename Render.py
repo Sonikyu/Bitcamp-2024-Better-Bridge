c0ddfa501bc85f660db6934e05f58b0c5f9bb999
JPR3 <jpr3@riganati.com>
2024-04-20 13:13:17 -0400

    Added function to draw hand

--- Render.py
+++ Render.py
@@ -5,11 +5,15 @@ import os
 import pygame
 pygame.init()
 
-SCREEN_WIDTH = 750
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 750
+CARD_WIDTH = 100
+CARD_HEIGHT = 145
+HAND_Y = SCREEN_HEIGHT - (CARD_HEIGHT + 20)
 
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_WIDTH])
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-def draw():
+def draw(hand):
     running = True
     while running:
         for event in pygame.event.get():
@@ -19,6 +23,11 @@ def draw():
         #Draw the background
         screen.fill((27, 99, 46))
         #Draw the player hand
+        hand_offset = (SCREEN_WIDTH - ((len(hand) + 1) * (CARD_WIDTH * 2 / 3))) / 2
+        loc_offset = 0
+        for card in hand:
+            draw_card(screen, card, hand_offset + loc_offset*CARD_WIDTH * 2/3, HAND_Y)
+            loc_offset += 1
         #Draw the pile
         #Draw the trump suit/bet
         #Draw the score (rounds won)
@@ -38,3 +47,9 @@ def get_png_from_card(card):
     output += card.suit.name.lower()
     output += ".png"
     return os.path.join("Cards", output)
+
+#Draws an individual card at x,y
+def draw_card(screen, card, x, y):
+    img_path = get_png_from_card(card)
+    img = pygame.image.load(img_path)
+    screen.blit(img, (x,y))
