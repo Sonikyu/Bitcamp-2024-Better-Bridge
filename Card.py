380672e9d79b3949cd1ab3c999f02c2a0752b819
Max Park <parkr.m465@gmail.com>
2024-04-20 00:17:18 -0400

    Update Card.py

--- Card.py
+++ Card.py
@@ -7,9 +7,15 @@ class Card:
         self.suit = suit
         self.id = suit.value * 13 + rank.value - 2
 
+    #Checks if the other card is bigger than or less than the current card
+    def __lt__ (self, otherCard):
+        return self.id - otherCard.id
+    
     def getRank(self):
         return self.rank
+    
     def getSuit(self):
         return self.suit
+    
     def toString(self):
         return self.rank + " of " + self.suit
